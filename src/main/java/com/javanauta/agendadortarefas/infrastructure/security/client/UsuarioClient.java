84dd0f0020728ee36cfbd2cfc5f09fe03cd6c36d
Gregory <gregory_gr@outlook.com>
2025-10-02 10:38:00 -0300

    add novas funcionalidades

--- src/main/java/com/javanauta/agendadortarefas/infrastructure/security/client/UsuarioClient.java
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/security/client/UsuarioClient.java
@@ -1,4 +1,4 @@
-package com.javanauta.agendadortarefas.infrastructure.client;
+package com.javanauta.agendadortarefas.infrastructure.security.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import com.javanauta.agendadortarefas.business.dto.UsuarioDTO;
